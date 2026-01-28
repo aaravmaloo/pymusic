5e63ea6ac0958a628193de955d727bbdd9275413
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-28 19:24:07 +0530

    create init.py for pypi prod

---
+++ pymusic/__init__.py
@@ -0,0 +1,8 @@
+from .core.pitch import Note, Pitch, Scale
+from .core.time import TimeContext
+from .composition.pattern import Pattern
+from .composition.chord import Chord
+from .engine.audio_graph import Song, Track
+from .engine.renderer import Renderer
+
+__version__ = "0.1.0"
