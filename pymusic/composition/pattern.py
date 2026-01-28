172899ca84622871e7e909279c7c2d9a50d4bc21
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-28 19:24:45 +0530

    Add Pattern class for musical sequence handling

---
+++ pymusic/composition/pattern.py
@@ -0,0 +1,33 @@
+from typing import List, Union, Optional
+from ..core.pitch import Note, Pitch, Rest
+
+class Pattern:
+    def __init__(self, elements: List[Union[Note, str, Pitch, None]] = None, loop: bool = False, length_beats: Optional[float] = None):
+        self.elements = []
+        self.loop = loop
+        self._explicit_length = length_beats
+        if elements:
+            for el in elements:
+                if isinstance(el, (Note, Pitch, Rest)):
+                    self.elements.append(el)
+                elif isinstance(el, str):
+                    self.elements.append(Note(el))
+                elif el is None:
+                    self.elements.append(Rest(1.0))
+                else:
+                    self.elements.append(el)
+                    
+    @property
+    def duration(self) -> float:
+        if self._explicit_length is not None:
+            return self._explicit_length
+        return sum(el.duration for el in self.elements if hasattr(el, 'duration'))
+
+    def add_note(self, pitch: Union[str, Pitch], duration: float = 1.0, velocity: float = 0.8):
+        self.elements.append(Note(pitch, duration, velocity))
+
+    def add_rest(self, duration: float = 1.0):
+        self.elements.append(Rest(duration))
+
+    def __repr__(self):
+        return f"Pattern(len={len(self.elements)})"
